3de4898743f9e7f2eefd4ae542dc70be06c4c8de
shchemelinin <shchemelinin@ya.ru>
2021-03-06 12:32:11 +0300

    negative fixed

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-#simple div
+#my calc
 
 str_command = input("Please type command a + b or a-b: ")
 
@@ -6,35 +6,30 @@ str_A = ''
 str_B = ''
 operation = ''
 '''
-2+3
-2 ^ 3
-0.5 - 4
+-2.5 ^ -3
 '''
+i = 0
 for letter in str_command:
-	print(letter)
-	if letter == '+' or letter =='-' or letter == '*' or letter == '/' or letter == '^':
+	if (letter == '+' or letter == '-' or letter == '*' or letter == '/' or letter == '^')\
+			and (operation == '') and (i > 0):
 		operation = letter
 	else:
 		if operation == '':
 			str_A = str_A + letter
 		else:
 			str_B = str_B + letter
+	i += 1
+
 str_A = str_A.strip()
 str_B = str_B.strip()
-print(str_A)
-print(str_B)
 
 #str_input = input("A: ")
 
 delimoe = float(str_A)
-#print(type(delimoe))
-
 #operation = input("+ / * - ^: ")
-
 #str_input2 = input("B: ")
 
 delitel = float(str_B)
-#print(type(delitel))
 result = None
 
 if operation == '/':
@@ -52,6 +47,5 @@ elif operation == '^':
 	result = delimoe ** delitel
 else:
 	result = "unknown"
-#print(type(result))
 
 print("Result: " + str(result))
