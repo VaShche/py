f601ab0d1ac7df665350df1162f644e4839598a5
shchemelinin <shchemelinin@ya.ru>
2021-03-09 19:36:19 +0300

    несколько операций без работающих приоритетов

--- calc.py
+++ calc.py
@@ -3,34 +3,44 @@
 str_command = input("Please type command a + b or a-b: ")
 str_command.replace(' ', '')
 
+'''
+parsing
+'''
+
 str_A = ''
 str_B = ''
-operation = ''
-'''
--2.5^-3
-'''
+variables = ['']
+operations = []
 
 for i, letter in enumerate(str_command):
-	if letter in '+-*/^' and (operation == '') and (i > 0):
-		operation = letter
+	if letter in '+-*/^' and (i > 0) and variables[len(operations)] != '':
+		operations.append(letter)
+		variables.append('')
 	else:
-		if operation == '':
-			str_A = str_A + letter
+		index = len(operations)
+		variables[index] = variables[index] + letter
+
+'''
+calculation
+'''
+variables = list(map(float, variables))
+result = variables[0]
+
+for i, operation in enumerate(operations):
+	if type(result) == str:
+		break
+
+	var_A = result
+	var_B = variables[i + 1]
+
+	if operation in '+-*/':
+		if var_B == 0 and operation == '/':
+			result = 'Inf'
 		else:
-			str_B = str_B + letter
-
-var_A = float(str_A)
-var_B = float(str_B)
-result = None
-
-if operation in '+-*/':
-	if var_B == 0 and operation == '/':
-		result = 'Inf'
+			result = eval('{0}{1}{2}'.format(var_A, operation, var_B))
+	elif operation == '^':
+		result = var_A ** var_B
 	else:
-		result = eval('{0}{1}{2}'.format(var_A, operation, var_B))
-elif operation == '^':
-	result = var_A ** var_B
-else:
-	result = "unknown"
+		result = "unknown"
 
 print("Result: " + str(result))
