c1381f9635b35c840cb99cb35bf7f62fe56b8a7c
shchemelinin <shchemelinin@ya.ru>
2021-03-09 18:52:47 +0300

    Так выглядит поприятней.

--- calc.py
+++ calc.py
@@ -1,50 +1,35 @@
 #my calc
 
 str_command = input("Please type command a + b or a-b: ")
+str_command.replace(' ', '')
 
 str_A = ''
 str_B = ''
 operation = ''
 '''
--2.5 ^ -3
+-2.5^-3
 '''
-i = 0
-for letter in str_command:
-	if (letter == '+' or letter == '-' or letter == '*' or letter == '/' or letter == '^')\
-			and (operation == '') and (i > 0):
+
+for i, letter in enumerate(str_command):
+	if letter in '+-*/^' and (operation == '') and (i > 0):
 		operation = letter
 	else:
 		if operation == '':
 			str_A = str_A + letter
 		else:
 			str_B = str_B + letter
-	i += 1
 
-str_A = str_A.strip()
-str_B = str_B.strip()
-
-#str_input = input("A: ")
-
-delimoe = float(str_A)
-#operation = input("+ / * - ^: ")
-#str_input2 = input("B: ")
-
-delitel = float(str_B)
+var_A = float(str_A)
+var_B = float(str_B)
 result = None
 
-if operation == '/':
-	if delitel == 0:
+if operation in '+-*/':
+	if var_B == 0 and operation == '/':
 		result = 'Inf'
 	else:
-		result = delimoe / delitel
-elif operation == '+':
-	result = delimoe + delitel
-elif operation == '-':
-	result = delimoe - delitel
-elif operation == '*':
-	result = delimoe * delitel
+		result = eval('{0}{1}{2}'.format(var_A, operation, var_B))
 elif operation == '^':
-	result = delimoe ** delitel
+	result = var_A ** var_B
 else:
 	result = "unknown"
 
