1400c123b60b67e440d9575b3ea4f94091645f0c
shchemelinin <shchemelinin@ya.ru>
2021-03-17 01:32:11 +0300

    range, tuple and dict added

--- calc.py
+++ calc.py
@@ -1,37 +1,51 @@
-#my calc
+# my calc
 
 str_command = input("Please type command a + b or a-b: ")
+# str_command = input()
 str_command.replace(' ', '')
 
 '''
 parsing
 '''
 
+hp_ops = tuple('^')
+mp_ops = tuple('*/')
+lp_ops = ('+', '-')
+supported_ops = hp_ops + mp_ops + lp_ops
+digits_chars = tuple(map(str, range(10))) + tuple('.,-')
+
 str_A = ''
 str_B = ''
-variables = ['']
-operations = []
+actions = []
+
+d = dict()
+d['opr'] = 'None'
+d['val'] = ''
+actions.append(d)
 
 for i, letter in enumerate(str_command):
-	if letter in '+-*/^' and (i > 0) and variables[len(operations)] != '':
-		operations.append(letter)
-		variables.append('')
-	else:
-		index = len(operations)
-		variables[index] = variables[index] + letter
+	if letter in supported_ops and (i > 0) and actions[-1].get('val') != '':
+		actions.append({'opr': letter,
+						'val': ''})
+	elif letter in digits_chars:
+		actions[-1]['val'] = actions[-1].get('val') + letter
 
 '''
 calculation
 '''
-variables = list(map(float, variables))
-result = variables[0]
+#variables = list(map(float, variables))
+for action in actions:
+	action['val'] = float(action.get('val'))
 
-for i, operation in enumerate(operations):
+result = actions[0].get('val')
+
+for action in actions:
 	if type(result) == str:
 		break
-
+	print(action)
 	var_A = result
-	var_B = variables[i + 1]
+	var_B = action.get('val')
+	operation = action.get('opr')
 
 	if operation in '+-*/':
 		if var_B == 0 and operation == '/':
@@ -41,6 +55,6 @@ for i, operation in enumerate(operations):
 	elif operation == '^':
 		result = var_A ** var_B
 	else:
-		result = "unknown"
+		result = var_A
 
 print("Result: " + str(result))
