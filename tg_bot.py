9d3cab7ae8b2aff9bf2acfa8953d86f89ffba5d4
shchemelinin <shchemelinin@ya.ru>
2021-03-17 16:43:26 +0300

    Украшательства для бота

--- tg_bot.py
+++ tg_bot.py
@@ -9,11 +9,25 @@ script = settings.script
 
 bot = telebot.TeleBot(settings.token)
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, '''Я умею считать выражения без скобок с операциями: + - * / ^
+Просто напишите мне например:
+5 + 6.4 / 2
+Если я не пойму, я промолчу :)''')
+
 @bot.message_handler(content_types=['text'])
 def get_text_msg(msg):
     if True:
-        print(msg.text)
+        #print(msg)
+        print(msg.from_user.username)
+        bot.send_chat_action(msg.from_user.id, 'typing')
         res = ''.join(script_executer([msg.text])[0])
-        bot.send_message(msg.from_user.id, res)
+        print(res)
+        if 'Result: ' in res:
+            #bot.send_message(msg.from_user.id, '@{0}\n{1}\n{2}'.format(msg.from_user.username, msg.text, res))
+            bot.reply_to(msg, res)
+        else:
+            pass
 
 bot.polling(none_stop=True)
